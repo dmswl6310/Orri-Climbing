4bea727a812f718b3a87bd0a2e731cbab7c4a7c1
dmswl6310 <dmswl6310@gmail.com>
2026-01-22 16:12:47 +0900

    chore: alias 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import Footer from "../components/layout/Footer";
-import Navbar from "../components/layout/Navbar";
+import Navbar from "@/components/layout/Navbar";
 import "./globals.css";
 import localFont from "next/font/local";
+import Footer from "@/components/layout/Footer";
 
 const pretendard = localFont({
   src: "../../public/fonts/PretendardVariable.woff2",
